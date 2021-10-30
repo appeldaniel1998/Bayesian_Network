0eef5aa715c9379c337f082e01ee5f4288208f71
Daniel Appel <appel.daniel1998@gmail.com>
2021-10-31 00:49:45 +0300

    Initial commit

---
+++ src/FileRead.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class FileRead {
+    public static void main(String[] args) {
+        xmlParser(fileReaderToLinkedList("src/Assignment/alarm_net.xml"));
+    }
+
+    private static void xmlParser(LinkedList<String> ls) {
+        int i = 0;
+        String name;
+        LinkedList<String> outcomes = new LinkedList<String>();
+        String line = ls.get(i);
+
+        //finding sections describing VARIABLES
+        if (line.equals("<VARIABLE>")) {
+            i++; //read the line of the opening <VARIABLE> tag
+            while (!line.equals("</VARIABLE>")) {
+                line = ls.get(i);
+
+                if (line.substring(0, 7).equals("\t<NAME>")) {
+                    name = line.substring(7, line.length() - 7);
+                }
+
+                if (line.substring(0, 10).equals("\t<OUTCOME>")) {
+                    outcomes.addLast(line.substring(10, line.length() - 10));
+                }
+
+                i++;
+            }
+        }
+
+
+    }
+
+
+    private static LinkedList<String> fileReaderToLinkedList(String str) {
+        LinkedList<String> ls = new LinkedList<String>();
+        Scanner sc = null;
+        try {
+            sc = new Scanner(new File(str));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        while (sc.hasNext()) {
+            ls.add(sc.nextLine());
+        }
+        return ls;
+    }
+
+
+}
---
+++ src/NetworkNode.java
@@ -0,0 +1,11 @@
+public class NetworkNode {
+    private String name;
+    private String[] outcomes;
+
+
+    public NetworkNode(String name, String[] outcomes)
+    {
+        this.name = name;
+        this.outcomes = outcomes;
+    }
+}
