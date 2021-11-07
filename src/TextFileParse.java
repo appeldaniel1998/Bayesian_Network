4ae6c1cbe1b4a7ba942017e834629916a61a58c6
Daniel Appel <appel.daniel1998@gmail.com>
2021-11-08 00:22:51 +0200

    Text parser done

--- src/TextFileParse.java
+++ src/TextFileParse.java
@@ -1,12 +1,7 @@
 import java.util.LinkedList;
 
 public class TextFileParse {
-    public static void main(String[] args) {
-        textParse("src/Assignment/input.txt");
-    }
-
-
-    public static void textParse(String filePath) {
+    public static LinkedList<Query> textParse(String filePath) {
         LinkedList<String> ls = Utilities.fileReaderToLinkedList(filePath);
         LinkedList<NetworkNode> nodes = XmlFileParse.xmlParser("src/Assignment/" + ls.get(0));
         LinkedList<Query> finalQueries = new LinkedList<Query>();
@@ -23,6 +18,7 @@ public class TextFileParse {
                 finalQueries.addLast(BayesParse(line, nodes));
             }
         }
+        return finalQueries;
     }
 
 
@@ -30,7 +26,7 @@ public class TextFileParse {
         int indexOpenBracket = 1; //for the sake of clarity in later uses. Unchanged throughout the code
         int indexCloseBracket = line.indexOf(')');
         String givenOutcomesAndQuery = line.substring(indexOpenBracket + 1, indexCloseBracket);
-        String[] splitByDefinition = givenOutcomesAndQuery.split("|"); //should return an array with size 2
+        String[] splitByDefinition = givenOutcomesAndQuery.split("\\|"); //should return an array with size 2
         String[] queryNodeAndValue = splitByDefinition[0].split("=");
 
         NetworkNode queryNode = Utilities.searchNode(nodes, queryNodeAndValue[0]);
@@ -52,10 +48,15 @@ public class TextFileParse {
     }
 
     private static BayesBallQuery BayesParse(String line, LinkedList<NetworkNode> nodes) {
-        String[] splitByDefinition = line.split("|");
+        String[] splitByDefinition = line.split("\\|");
         NetworkNode queryNode1 = Utilities.searchNode(nodes, splitByDefinition[0].substring(0, 1));
         NetworkNode queryNode2 = Utilities.searchNode(nodes, splitByDefinition[0].substring(2, 3));
-        Object[] arr = givenListParse(nodes, splitByDefinition[1]);
+        Object[] arr = new Object[0];
+        try {
+            arr = givenListParse(nodes, splitByDefinition[1]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            arr = givenListParse(nodes, "");
+        }
         NetworkNode[] givenNodesArr = (NetworkNode[]) arr[0];
         String[] givenValuesArr = (String[]) arr[1];
 
@@ -101,32 +102,3 @@ public class TextFileParse {
         return ret;
     }
 }
-
-/*
-//splitting given pairs (node + variable), each of them splitting by "=" and adding each to the appropriate list
-        //for future use and initialization of object
-        if (splitByDefinition[1].length() != 0) {
-            if (splitByDefinition[1].contains(",")) { //multiple pairs of nodes and values
-                String[] givenPairs = splitByDefinition[1].split(",");
-                for (int j = 0; j < givenPairs.length; j++) {
-                    String[] temp = givenPairs[j].split("="); //array of size 2
-
-                    NetworkNode node = Utilities.searchNode(nodes, temp[0]);
-                    givenNodes.addLast(node);
-
-                    givenValues.addLast(temp[1]);
-                }
-                givenNodesArr = Utilities.linkedListToArrayNodes(givenNodes);
-                givenValuesArr = Utilities.linkedListToArray(givenValues);
-            }
-            else //only one pair of node and value
-            {
-                String[] temp = splitByDefinition[1].split("=");
-                NetworkNode node = Utilities.searchNode(nodes, temp[0]);
-                givenNodesArr = new NetworkNode[1];
-                givenNodesArr[0] = node;
-                givenValuesArr = new String[1];
-                givenValuesArr[0] = temp[1];
-            }
-        }
- */
